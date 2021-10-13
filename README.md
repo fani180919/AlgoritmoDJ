149f6848e80a32474c59900db18ece9c1efdeb70
Noe VG <noe-vg@outlook.com>
2021-10-13 13:49:38 -0500

    add:the structure of the code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dijkstra's algorithm
+
+En este repositorio se codifcara el algoritmo de Dijkstra's algorithm, en lenguaje Java.
+
---
+++ java/AlgoritmoDJ.java
@@ -0,0 +1,26 @@
+/**
+ * @author: Ing. Noé Vásquez Godínez
+ * @email: noe-vg@outlook.com
+ * @about: Clase para implementar Dijkstra's algorithm
+ */
+class AlgoritmoDJ{
+    /**
+     * @about:
+     * En este metodo se iniciara sel setup del algoritmo
+     */
+    public void init(){
+        this.start();
+    }
+    /**@about: 
+     * En este metodo se incia el algoritmo
+     */
+    private void start(){
+        // . . .
+        // . . .
+        // . . .
+    } 
+    public static void main(String[] args) {
+        AlgoritmoDJ algoritmoDJ = new AlgoritmoDJ();
+        algoritmoDJ.init();
+    }
+}
