32c9a422bf681bf0caba9725ee299eb2cc63b720
fani180919 <martinez.fani.1fm.18@gmail.com>
2021-10-19 14:21:51 -0500

    add my code

---
+++ AlgoritmoDJ.java
@@ -0,0 +1,22 @@
+]/*Clase para implementar el Dijkstra's algorithm
+* @author: Martínez Meza Fani :)
+* @about: Clase para implementar    DJ Algorithm 
+*/
+class AlgoritmoDJ{
+    /**En este momento se iniciara el sel set up del algoritmo */
+public void init(){
+this .start();
+}
+/**en este metodo se inicia el algoritmo */
+private void start (){
+//. . .
+//. . .
+//. . .
+System.out.print("Hola estrellitas");
+}
+public static void main (string[]args){
+AlgoritmoDJ algoritmoDJ= new AlgoritmoDJ();
+algoritmoDJ.init();
+
+}
+}
